37d0603c76c32e4cf80c49e08b4744a33075aba6
unknown <ilhamsetyo315@gmail.com>
2026-07-03 10:42:59 +0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react', 'react-dom', 'framer-motion', 'lucide-react', 'react-scroll', 'react-countup']
+    include: ['react', 'react-dom', 'framer-motion', 'lucide-react', 'react-scroll', 'react-countup'],
+    base: proccess.env.VITE_BASE_PATH || "/portfolio-web",
   }
 })
